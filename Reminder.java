0c7f88b3a2147b1e3e07d1b728b0a24c7cfb87a4
Natalie Pinto <36931962+natalieppinto97@users.noreply.github.com>
2018-05-01 15:35:49 -0400

    Update Reminder.java

--- Reminder.java
+++ Reminder.java
@@ -1,6 +1,7 @@
 import java.util.TimerTask;
-
+//This class extends the timer class.
 class Reminder extends TimerTask {
+	//This method allows the mesage to be displayed every time the timer goes off.
 	public void run() {
 		System.out.println("You need to drink more water");
 	}
